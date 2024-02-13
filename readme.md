185d0283943bbbc2d99af7b115c8b7d400203a34
zhuchen_pig <zhuchen_pig233@outlook.com>
2024-02-13 21:15:15 +0800

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# A abstract minecraft mod.
+As you see, this is a abstract mod in MineCraft for Chinese player. In this mod, there are some Chinese memes.
+
+> The mod is developing. Stay tuned.
---
+++ src/main/java/com/dropper/AbstractModClient.java
@@ -0,0 +1,10 @@
+package com.dropper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AbstractModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
